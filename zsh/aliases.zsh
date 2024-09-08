6a7ded8746f11050fbb47b7e930e28254a57e66a
Kyle Beede <kyle.beede@faithlife.com>
2024-09-08 00:05:17 -0700

    Add/update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,5 @@
-# need to escape this alias so it doesn't get expanded until run
-# EDITOR is set after sourcing this file
-alias zshrc="\$EDITOR ~/.dotfiles/zsh/zshrc.symlink"
-alias ohmyzsh="\$EDITOR ~/.dotfiles/oh-my-zsh"
+alias zshrc="$EDITOR ~/.dotfiles/zsh/zshrc.symlink"
+alias ohmyzsh="$EDITOR ~/.dotfiles/oh-my-zsh"
+
+alias reload!='. ~/.zshrc'
+alias cls='clear'
