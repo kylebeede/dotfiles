c8d0664973321abdef89467f3eccbeaa57b335e8
Kyle Beede <kyle.beede@faithlife.com>
2024-12-04 11:55:16 -0800

    Fix find in directory command

--- fzf/aliases.zsh
+++ fzf/aliases.zsh
@@ -1,10 +1,17 @@
-# open fzf-selected files in Neovim
-fzf_open_in_nvim() {
-  local files
-  files=$(fzf --multi)  # Allow multiple file selection
-  if [[ -n "$files" ]]; then
-    nvim $files
+function fzf_open_in_nvim() {
+  local dir="${1:-.}"
+  local selected_files
+
+  if [ ! -d "$dir" ]; then
+    echo "Error: '$dir' is not a valid directory"
+    return 1
+  fi
+
+  selected_files=$(cd "$dir" && find . -type f | sed 's|^\./||' | fzf --multi)
+
+  if [ -n "$selected_files" ]; then
+    echo "$selected_files" | xargs -I {} nvim "$dir/{}"
   fi
 }
 
-alias fn="fzf_open_in_nvim"
+alias fid="fzf_open_in_nvim"
