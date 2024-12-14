e9ae0ac9b81442ac1d7f50464d4a7f00125f62a7
Kyle Beede <kyle.beede@faithlife.com>
2024-12-13 16:39:12 -0800

    Don't use hub

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,9 +1,9 @@
 # Use `hub` as git wrapper:
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
+# hub_path=$(which hub)
+# if (( $+commands[hub] ))
+# then
+#   alias git=$hub_path
+# fi
 
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin master'
